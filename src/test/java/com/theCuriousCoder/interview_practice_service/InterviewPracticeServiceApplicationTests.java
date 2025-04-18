4e45649fbf9e06895774bb3bc34dd547efcb01d0
TheCuriousCoder-sg <tech.learning9868@gmail.com>
2025-04-19 00:13:46 +0530

    Initial Commit

---
+++ src/test/java/com/theCuriousCoder/interview_practice_service/InterviewPracticeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theCuriousCoder.interview_practice_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPracticeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
