3cbae4b5a85e83b09ea7eb1a234657250cc767df
TheCuriousCoder-sg <tech.learning9868@gmail.com>
2025-05-13 22:07:10 +0530

    Lombok Changes

---
+++ src/main/java/com/theCuriousCoder/interview_practice_service/Employee.java
@@ -0,0 +1,11 @@
+package com.theCuriousCoder.interview_practice_service;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+public class Employee {
+    private Long id;
+    private String name;
+
+}
