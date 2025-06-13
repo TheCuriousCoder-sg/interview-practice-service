37a284d1bcdca334266cfc3332707e49997b4be3
TheCuriousCoder-sg <tech.learning9868@gmail.com>
2025-06-13 18:27:02 +0530

    environment properties

---
+++ src/main/java/com/theCuriousCoder/interview_practice_service/Employee.java
@@ -0,0 +1,12 @@
+package com.theCuriousCoder.interview_practice_service;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile({"dev", "test"})
+public class Employee {
+    public Employee() {
+        System.out.println("This is employee bean");
+    }
+}
